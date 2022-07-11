b0d6ad1189fded0c3cb3748617148e0150e5be46
AVATLY <109079612+AVATLY@users.noreply.github.com>
2022-07-11 15:47:51 +0200

    Initial commit

---
+++ AVATLY.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts@4.6.0/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts@4.6.0/access/Ownable.sol";
+
+/// @custom:security-contact jjablczynski@avatly.com
+contract AVATLY is ERC20, Ownable {
+    constructor() ERC20("AVATLY", "AVA") {
+        _mint(msg.sender, 1000000000 * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
